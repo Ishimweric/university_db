9d4172efe632922178b997b95e8d943297730c09
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-24 09:13:35 +0200

    add: Added a query to count how many students in each course

--- queries.sql
+++ queries.sql
@@ -17,7 +17,7 @@ JOIN
 WHERE
     c.course_code = 'CS101';
 
--- Query 2 : retrieve all courses that a specific student is enrolled in
+-- Query 2 : retrieve all courses that a specific student is enrolled in based on email
 SELECT
     s.name AS student_name,
     s.email AS student_email,
@@ -32,3 +32,17 @@ JOIN
     courses c ON e.course_id = c.id
 WHERE
     s.email = 'alice.smith@example.com';
+
+-- query 3: Return a count of how many students are enrolled in each course
+SELECT
+    c.course_name,
+    c.course_code,
+    COUNT(e.student_id) AS number_of_students_enrolled
+FROM
+    courses c
+LEFT JOIN -- used LEFT JOIN to include courses with 0 enrollments also
+    enrollments e ON c.id = e.course_id
+GROUP BY
+    c.course_name, c.course_code
+ORDER BY
+    number_of_students_enrolled DESC, c.course_code ASC;

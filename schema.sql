711434ce88261d252e6ede7b626f8cb877906df3
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-24 08:23:28 +0200

    add: Created students table

--- schema.sql
+++ schema.sql
@@ -1,2 +1,14 @@
 -- create database if it don't exist
 CREATE DATABASE IF NOT EXISTS university_db;
+
+-- use created database
+USE university_db;
+
+-- 1. Create the students table
+CREATE TABLE students (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL UNIQUE,
+  dob DATE,
+  program VARCHAR(50)
+);

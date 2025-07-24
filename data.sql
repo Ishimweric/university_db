1b5c5c016e09b68149f8ebe71ccb6c1f6c571763
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-24 08:55:21 +0200

    add: Added some data in enrollments table

--- data.sql
+++ data.sql
@@ -14,3 +14,60 @@ INSERT INTO courses (course_name, course_code, instructor) VALUES
 ('Data Structures & Algorithms', 'CS201', 'Prof. Alan Turing'),
 ('Circuit Design Fundamentals', 'EE101', 'Dr. Nikola Tesla'),
 ('Linear Algebra', 'MA201', 'Prof. Emmy Noether');
+
+-- Insert enrollment records using students and courses attributes
+
+-- for alice
+INSERT INTO enrollments(student_id, course_id, enrollment_date) VALUES
+(
+  (SELECT id FROM students WHERE email = 'alice.smith@example.com'), 
+  (SELECT id FROM courses WHERE course_id = 'CS101'),
+  '2025-11-11'
+),
+(
+  (SELECT id FROM students WHERE email = 'alice.smith@example.com'), 
+  (SELECT id FROM courses WHERE course_id = 'CS102'),
+  '2025-11-11'
+);
+
+-- for bob
+INSERT INTO enrollments (student_id, course_id, date_enrolled) VALUES
+(
+  (SELECT id FROM students WHERE email = 'bob.j@example.com'), 
+  (SELECT id FROM courses WHERE course_code = 'EE101'), 
+  '2024-09-01'
+),
+(
+  (SELECT id FROM students WHERE email = 'bob.j@example.com'), 
+  (SELECT id FROM courses WHERE course_code = 'MA201'), 
+  '2024-09-05'
+);
+
+--for charlie
+INSERT INTO enrollments (student_id, course_id, date_enrolled) VALUES
+(
+  (SELECT id FROM students WHERE email = 'charlie.b@example.com'), 
+  (SELECT id FROM courses WHERE course_code = 'CS101'), 
+  '2024-09-02'
+);
+
+--Diana
+INSERT INTO enrollments (student_id, course_id, date_enrolled) VALUES
+(
+  (SELECT id FROM students WHERE email = 'diana.p@example.com'), 
+  (SELECT id FROM courses WHERE course_code = 'CS101'), 
+  '2024-09-01'
+),
+(
+  (SELECT id FROM students WHERE email = 'diana.p@example.com'), 
+  (SELECT id FROM courses WHERE course_code = 'CS201'), 
+  '2024-09-01'
+);
+
+--FOR eve
+INSERT INTO enrollments (student_id, course_id, date_enrolled) VALUES
+(
+  (SELECT id FROM students WHERE email = 'eve.a@example.com'), 
+  (SELECT id FROM courses WHERE course_code = 'MA201'), 
+  '2024-09-03'
+);
